a7b24f792969ed61d6b1e4ceef944dbe5f627219
Abhishek Singh <115094053+work-abhisheksingh@users.noreply.github.com>
2023-03-02 10:38:22 +0530

    Update SortColors.java

--- SortColors.java
+++ SortColors.java
@@ -1,31 +1,6 @@
 package array;
-
-/**
- * Created by gouthamvidyapradhan on 06/08/2017. Given an array with n objects colored red, white or
- * blue, sort them so that objects of the same color are adjacent, with the colors in the order red,
- * white and blue.
- *
- * <p>Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue
- * respectively.
- *
- * <p>Note: You are not suppose to use the library's sort function for this problem.
- *
- * <p>Follow up: A rather straight forward solution is a two-pass algorithm using counting sort.
- * First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total
- * number of 0's, then 1's and followed by 2's.
- *
- * <p>Could you come up with an one-pass algorithm using only constant space?
- *
- * <p>Solution: The below solution works with one pass. The basic idea is to keep track of start and
- * end index of contiguous 1s and push the 0s to left of 1s and 2 to right of 1s.
- */
 public class SortColors {
-  /**
-   * Main method
-   *
-   * @param args
-   * @throws Exception
-   */
+  
   public static void main(String[] args) throws Exception {
     int[] nums = {2, 1, 0, 0, 1};
     new SortColors().sortColors(nums);
