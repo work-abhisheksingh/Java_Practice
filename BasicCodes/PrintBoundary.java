3727fc9321c3c76be76c044c46130dd895ddd5a6
Abhishek Singh <115094053+work-abhisheksingh@users.noreply.github.com>
2023-06-12 10:16:50 +0530

    Create PrintBoundary.java

---
+++ BasicCodes/PrintBoundary.java
@@ -0,0 +1,48 @@
+package BasicCodes;
+import java.util.Scanner;
+
+
+public class PrintBoundaryElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter the matrix elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("The matrix is:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("The boundary elements are:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0 || j == 0 || i == rows - 1 || j == columns - 1) {
+                    System.out.print(matrix[i][j] + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
